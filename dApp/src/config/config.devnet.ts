47f54e95f2f1f42c7341066c4c0432a685cfb7f3
eminesa <eminesa34@gmail.com>
2024-12-29 20:20:49 +0300

    Added dapp without connection to the main dapp

---
+++ dApp/src/config/config.devnet.ts
@@ -0,0 +1,9 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgq06sxx4zhattlzn4vkfz9rcmak9usrfajj02qdtugnv';
+export const API_URL = 'https://devnet-template-api.multiversx.com';
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.devnet;
